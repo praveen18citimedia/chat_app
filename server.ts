16bfb33c6085ab4bd4390ba0fb31b379cd95c97e
praveen18citimedia <praveen18.citimedia@gmail.com>
2024-04-24 16:16:38 +0530

    initial commit

---
+++ server.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import path from "path"
+import { Server, Socket } from "socket.io";
+
+const app=express()
+const port =2000
+const server=app.listen(port,()=>{
+    console.log(`app is runnong at ${port}`)
+})
+
+app.use(express.static(path.join(__dirname,'public')))
+let socketsConnected=new Set()
+const io = new Server(server);
+io.on('connection',onConnection)
+function onConnection(socket:Socket){
+    console.log('socket connected', socket.id)
+socketsConnected.add(socket.id)
+io.emit('clints-total',socketsConnected.size)
+    socket.on('disconnect',()=>{
+        console.log('socket disconnected', socket.id)
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+    })
+    socket.on('message',(data)=>{
+        console.log(data);
+        socket.broadcast.emit('chat-message',data)
+    })
+    socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data)
+      })
+}
